eb3c0ea9eb0dae2d3d96b4c1ae02fea69655f3fe
fightingwenx <252801901@qq.com>
2019-04-07 12:36:44 +0800

    add setData

--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="movie_body">
         <ul>
-            <li>
+            <!-- <li>
                 <div class="pic_show"><img src="/images/movie_2.jpg"></div>
                 <div class="info_list">
                     <h2>无名之辈</h2>
                     <p>观众评<span class="grade">9.2</span></p>   
                     <p>主演：陈建斌</p>
                     <p>今天55aa家影院放映607场aa</p>                 
+                </div>
+                <div class="btn_mall">
+                    购票
+                </div>
+            </li> -->
+            <li v-for="item in comingList" :key="item.id">
+                <div class="pic_show"><img :src="item.img | setWH('128.180')"></div>
+                <div class="info_list">
+                    <h2>{{item.nm}} <img v-if="item.version" src="@/assets/imax.png" alt=""></h2>
+                    <p><span class="person">{{item.wish}}</span>人想看</p>   
+                    <p>主演：{{ item.star }}</p>
+                    <p>{{ item.rt}}上映</p>                 
                 </div>
                 <div class="btn_mall">
                     购票
@@ -19,7 +31,20 @@
 
 <script>
 export default {
-    name : 'ComingSoon'
+    name : 'ComingSoon',
+    data(){
+        return {
+            comingList:[]
+        };
+    },
+    mounted(){
+        this.axios.get('/api/movieComingList?cityId=10').then((res)=>{
+            var msg = res.data.msg;
+            if(msg==='ok'){
+                this.comingList=res.data.data.comingList;
+            }
+        })
+    }
 }
 </script>
 
